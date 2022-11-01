41adf1639ae48c3ede5e2bde71bcd8befcc50bdc
thorn1113 <1902492516@qq.com.cn>
2022-11-01 18:51:32 +0800

    q21

---
+++ src/leedcode/数据结构/day07/q21_合并两个链表/Solution.java
@@ -0,0 +1,27 @@
+package leedcode.数据结构.day07.q21_合并两个链表;
+
+public class Solution {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {
+        }
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+
+        return null;
+
+    }
+
+
+}
