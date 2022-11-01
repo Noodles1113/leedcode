e8f46ffd10a949f6eac924e63714356e3a1ae39e
thorn1113 <1902492516@qq.com.cn>
2022-11-01 14:13:58 +0800

    初始

---
+++ src/leedcode/两周数据结构入门/day06/q383_赎金信/Solution.java
@@ -0,0 +1,27 @@
+package leedcode.两周数据结构入门.day06.q383_赎金信;
+
+public class Solution {
+    public static boolean canConstruct(String ransomNote, String magazine) {
+        //记录杂志字符串出现的次数
+        int[] arr = new int[26];
+        int temp;
+        for (int i = 0; i < magazine.length(); i++) {
+            temp = magazine.charAt(i) - 'a';
+            arr[temp]++;
+        }
+        for (int i = 0; i < ransomNote.length(); i++) {
+            temp = ransomNote.charAt(i) - 'a';
+            if (arr[temp] > 0) {
+                arr[temp]--;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(canConstruct("aa", "zaa"));
+
+    }
+}
